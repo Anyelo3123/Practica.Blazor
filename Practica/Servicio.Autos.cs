69195b098661b4b05c786369bd72d7b3fdbaea42
Anyelo Rodriguez <Anyelo Rodriguez@DESKTOP-BQ5NA7K>
2025-02-07 21:18:05 +0100

    añadimos la logica para eliminar autos y agregar autos

---
+++ Practica/Servicio.Autos.cs
@@ -0,0 +1,29 @@
+﻿namespace Practica
+
+{
+    public class Servicio
+    {
+        private List<Car> cars = new List<Car>();
+
+        public List<Car> GetCars()
+        {
+            return cars;
+        }
+
+        public void AddCar(Car car)
+        {
+            car.Id = cars.Count > 0 ? cars.Max(c => c.Id) + 1 : 1;
+            cars.Add(car);
+        }
+
+        public void RemoveCar(int id)
+        {
+            var car = cars.FirstOrDefault(c => c.Id == id);
+            if (car != null)
+            {
+                cars.Remove(car);
+            }
+        }
+    }
+}
+
