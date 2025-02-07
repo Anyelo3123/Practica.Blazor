4dea0c03759410a03c680bbc2da278435f6c1c9e
Anyelo Rodriguez <Anyelo Rodriguez@DESKTOP-BQ5NA7K>
2025-02-07 21:11:52 +0100

    hasta ahora va el diseño para agregar autos y las propiedades para los vehiculos en la clase car

---
+++ Practica/Car.cs
@@ -0,0 +1,10 @@
+﻿namespace Practica
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int Año { get; set; }
+    }
+}
